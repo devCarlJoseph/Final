55619ff29c8b08d20e801dd373b2c311f8f68ad9
unknown <dj.degamo.p@gmail.com>
2025-04-19 11:11:23 +0800

    123

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,39 +10,6 @@ headerClose.addEventListener("click", () => {
     header.classList.add('hidden');
 })
 
-// const faq = document.getElementById('faqContent');
-// const faqTwo = document.getElementById('faqContentTwo');
-// const faqThree = document.getElementById('faqContentThree');
-// const faqFour = document.getElementById('faqContentFour');
-// const faqOpen = document.getElementById('faqOpen');
-// const faqOpenTwo = document.getElementById('faqOpenTwo');
-// const faqOpenThree = document.getElementById('faqOpenThree');
-// const faqOpenFour = document.getElementById('faqOpenFour');
-
-// faqOpen.addEventListener("click", () => {
-//     faqOpen.classList.toggle('fa-plus');
-//     faqOpen.classList.toggle('fa-minus');
-//     faq.classList.toggle('hidden');
-// });
-
-// faqOpenTwo.addEventListener("click", () => {
-//     faqOpenTwo.classList.toggle('fa-plus');
-//     faqOpenTwo.classList.toggle('fa-minus');
-//     faqTwo.classList.toggle('hidden');
-// });
-
-// faqOpenThree.addEventListener("click", () => {
-//     faqOpenThree.classList.toggle('fa-plus');
-//     faqOpenThree.classList.toggle('fa-minus');
-//     faqThree.classList.toggle('hidden');
-// });
-
-// faqOpenFour.addEventListener("click", () => {
-//     faqOpenFour.classList.toggle('fa-plus');
-//     faqOpenFour.classList.toggle('fa-minus');
-//     faqFour.classList.toggle('hidden');
-// });
-
 $(document).ready(function() {
     $('.js-faq-btn').on('click', function(e) {
         e.preventDefault();
